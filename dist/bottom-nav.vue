b643204160867b9a37a38e105489506212da658e
余宝彬 <502275531@qq.com>
2017-04-22 16:25:27 +0800

    update

---
+++ dist/bottom-nav.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bottom-nav" >
+  	<slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bottom-nav',
+  componentName: 'bottom-nav',
+  data () {
+    return {
+      activeIndex: ''
+    }
+  },
+  mounted () {
+    this.$on('itemClick', this.handleItemClick)
+  },
+  methods: {
+    handleItemClick (child) {
+      for (const item of this.$children) {
+        item.$data.activeIndex = child.index
+      }
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.bottom-nav {
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  flex-wrap: nowrap;
+  padding: 15px 0;
+}
+</style>
